2cd1d9941ffd7686885299ef792ef247713c7133
Shota Kishimoto <shokishimo@gmail.com>
2022-11-13 18:27:44 -0600

    Time: 8 ms (75.37%), Space: 42.4 MB (97.94%) - LeetHub

--- 0006-Zigzag-Conversion/0006-Zigzag-Conversion.java
+++ 0006-Zigzag-Conversion/0006-Zigzag-Conversion.java
@@ -1,29 +1,21 @@
 class Solution {
     public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+
         ArrayList<ArrayList<Character>> map = new ArrayList<>(numRows);
         for (int i = 0; i < numRows; i++)
         {
             map.add(new ArrayList<>());
         }
-
-        if (numRows > 1)
+        
+        int adjuster = 1;
+        int counter = 0;
+        for (int i = 0; i < s.length(); i++)
         {
-            int adjuster = 1;
-            int counter = 0;
-            for (int i = 0; i < s.length(); i++)
-            {
-                if (counter == numRows-1) adjuster = -1;
-                else if (counter == 0) adjuster = 1;
-                map.get(counter).add(s.charAt(i));
-                counter += adjuster;
-            }
-        } 
-        else 
-        {       
-            for (int i = 0; i < s.length(); i++)
-            {
-                map.get(0).add(s.charAt(i));
-            }
+            if (counter == numRows-1) adjuster = -1;
+            else if (counter == 0) adjuster = 1;
+            map.get(counter).add(s.charAt(i));
+            counter += adjuster;
         }
  
         StringBuilder sb = new StringBuilder();
