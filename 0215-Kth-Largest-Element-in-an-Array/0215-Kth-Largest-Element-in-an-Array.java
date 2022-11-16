cc7e5dec030281b4666cae1a6569f56a4338c5f3
Shota Kishimoto <shokishimo@gmail.com>
2022-11-16 00:50:18 -0600

    Time: 87 ms (38.97%), Space: 52.6 MB (84.14%) - LeetCodeHub

--- 0215-Kth-Largest-Element-in-an-Array/0215-Kth-Largest-Element-in-an-Array.java
+++ 0215-Kth-Largest-Element-in-an-Array/0215-Kth-Largest-Element-in-an-Array.java
@@ -5,3 +5,22 @@ class Solution {
         return nums[nums.length-k];
     }
 }
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            queue.add(nums[i]);
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            if (i == k-1) return queue.poll();
+            else queue.poll();
+        }
+
+        return -1;
+    }
+}
