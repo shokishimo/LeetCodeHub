da65c56c26ab857dea7bf16e873abc695e4a188d
Shota Kishimoto <shokishimo@gmail.com>
2023-01-31 19:23:30 -0600

    Time: 4 ms (77.87%) , Space 42.8 MB (67.60%) - LeetCode

---
+++ 10025-Valid-Palindrome/10025-Valid-Palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+  public boolean isPalindrome(String s) {
+    StringBuilder builder = new StringBuilder();
+
+    for (char ch : s.toCharArray()) {
+      if (Character.isLetterOrDigit(ch)) {
+        builder.append(Character.toLowerCase(ch));
+      }
+    }
+
+    String filteredString = builder.toString();
+    String reversedString = builder.reverse().toString();
+
+    return filteredString.equals(reversedString);
+  }
+}
