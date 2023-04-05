c3f1b3fb2163c1c1a5af59c66aec7fc25d7be1c5
Shota Kishimoto <shokishimo@gmail.com>
2023-04-05 00:51:26 -0500

    Time: 0 ms (100%), 39.5 MB (95.60%) - LeetCode

--- 0213-House-Robber-II/0213-House-Robber-II.java
+++ 0213-House-Robber-II/0213-House-Robber-II.java
@@ -32,3 +32,37 @@ class Solution {
       return sum;
   }
 }
+
+// solution 2 (More Clean)
+class Solution {
+  public int rob(int[] nums) {
+      if (nums.length == 1) return nums[0];
+      if (nums.length == 2) return Math.max(nums[0], nums[1]);
+      
+      int ans1 = 0;
+      int ans2 = 0;
+      
+      int first = 0;
+      int second = 0;
+      
+      for (int i = 0; i < nums.length-1; i++)
+      {
+          int dp = Math.max(nums[i]+first, second);
+          first = second;
+          second = dp;
+      }
+      ans1 = second;
+      
+      first = second = 0;
+      
+      for (int i = 1; i < nums.length; i++)
+      {
+          int dp = Math.max(nums[i]+first, second);
+          first = second;
+          second = dp;
+      }
+      ans2 = second;
+      
+      return Math.max(ans1, ans2);
+  }
+}
