41947743df9aee7d9a648ad707bba549c0f5f52b
Shota Kishimoto <shokishimo@gmail.com>
2023-04-12 04:56:24 -0500

    Time: 1 ms (100%), Space: 51.8 MB (40.29%) - LeetCode

---
+++ 0053-Maximum-Subarray/0053-Maximum-Subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int maxSubArray(int[] nums) {
+      int ans = nums[0];
+      int sum = nums[0];
+
+      for (int i = 1; i < nums.length; i++) {
+          sum = Math.max(nums[i], sum + nums[i]);
+          ans = Math.max(ans, sum);
+      }
+      
+      return ans;
+  }
+}
