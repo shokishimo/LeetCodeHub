941f1767161fe4f2ff399e3e8205a167ff80fd6f
Shota Kishimoto <shokishimo@gmail.com>
2023-04-16 20:42:02 -0500

    Time: 1 ms (93.79 %), Space: 42 MB (63.32%)- LeetCode

--- 0190-Reverse-Bits/0190-Reverse-Bits.java
+++ 0190-Reverse-Bits/0190-Reverse-Bits.java
@@ -1,6 +1,21 @@
+// Solution 1
 public class Solution {
   // you need treat n as an unsigned value
   public int reverseBits(int n) {
       return Integer.reverse(n);
+  }
+}
+
+// Solution 2
+public class Solution {
+  // you need treat n as an unsigned value
+  public int reverseBits(int n) {
+      int ans = 0;
+      for (int i = 0; i < 32; i++) {
+          int t = n & 1;
+          ans |= (t << (31 - i));
+          n = n >> 1;
+      }
+      return ans;
   }
 }
