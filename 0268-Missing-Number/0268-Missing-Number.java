73384cd79e3c41fa82fbc8e1584a33dd46b4d5d8
Shota Kishimoto <shokishimo@gmail.com>
2023-04-16 21:50:05 -0500

    Time: 0 ms (100 %), Space: 42.6 MB (98.46 %) - LeetCode

--- 0268-Missing-Number/0268-Missing-Number.java
+++ 0268-Missing-Number/0268-Missing-Number.java
@@ -28,3 +28,17 @@ class Solution {
         return ans;
     }
 }
+
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int ans = n * (n + 1) / 2;;
+        for (int i = 0; i < n; i++) {
+            ans -= nums[i];
+        }
+        
+        return ans;
+    }
+}
