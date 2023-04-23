2904a2bd4c0d342cca88151816e88f3beab1f843
Shota Kishimoto <shokishimo@gmail.com>
2023-04-22 23:55:40 -0500

    Time: 0 ms (100 %), Space: 41.2 MB (42.89 %)- LeetCode

--- 0066-Plus-One/0066-Plus-One.java
+++ 0066-Plus-One/0066-Plus-One.java
@@ -24,3 +24,37 @@ class Solution {
         return digits;
     }
 }
+
+
+// s2
+class Solution {
+    public int[] plusOne(int[] digits) {
+        boolean isCarry = true;
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != 9) {
+                isCarry = false;
+                break;
+            }
+        }
+
+        int[] ans;
+        if (isCarry) ans = new int[digits.length+1];
+        else ans = new int[digits.length];
+
+        int carry = 0;
+        for (int i = digits.length-1; i >= 0; i--) {
+            if (i == digits.length-1) {
+                if (isCarry) ans[i+1] = (digits[i] + 1)%10;
+                else ans[i] = (digits[i] + 1)%10;
+                carry = (digits[i] + 1)/10;
+            } else {
+                if (isCarry) ans[i+1] = (digits[i] + carry)%10;
+                else ans[i] = (digits[i] + carry)%10;
+                carry = (digits[i] + carry)/10;
+            }
+        }
+        if (isCarry) ans[0] = carry;
+
+        return ans;
+    }
+}
