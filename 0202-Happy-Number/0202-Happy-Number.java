68b6717fa034bd7f23aa17ad6dc8ba9b35a78422
Shota Kishimoto <shokishimo@gmail.com>
2023-04-24 01:40:12 -0500

    Time: 1ms (92.25%), Space: 40.3MB (19.79%) - LeetCode

--- 0202-Happy-Number/0202-Happy-Number.java
+++ 0202-Happy-Number/0202-Happy-Number.java
@@ -1,27 +1,30 @@
 class Solution {
     public boolean isHappy(int n) {
-        if (n == 1) return true;
-        HashSet<Integer> set = new HashSet<>();
-
-        boolean ans = false;
-        while (n != 1)
-        {
-            int sum = 0;
-            while (n > 0)
-            {
-                sum += (int) Math.pow(n%10, 2);
-                n = n/10;
-            }
-            if (sum == 1) {
+        HashSet<Integer> repeat = new HashSet<>();
+        repeat.add(n);
+        boolean ans = true;
+        while(n != 0) {
+            n = getSumSquares(n);
+            if (n == 1) {
                 ans = true;
                 break;
-            }
-            else if (!set.add(sum)) {
+            }else if (!repeat.add(n)) {
+                ans = false;
                 break;
             }
-            n = sum;
         }
 
         return ans;
+    }
+
+    public int getSumSquares(int n) {
+        int res = 0;
+        while (n != 0) {
+            int a = n%10;
+            res += a*a;
+            n /= 10;
+        }
+
+        return res;
     }
 }
