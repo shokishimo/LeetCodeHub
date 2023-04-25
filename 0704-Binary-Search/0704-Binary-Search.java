5ea8894ede8ce6c32c1962f8721bf16ce5ffeba6
Shota Kishimoto <shokishimo@gmail.com>
2023-04-25 13:10:23 -0500

    Time: 0 ms (100%), Space: 42.9 MB (84.46%)- LeetCode

--- 0704-Binary-Search/0704-Binary-Search.java
+++ 0704-Binary-Search/0704-Binary-Search.java
@@ -20,3 +20,23 @@ class Solution {
       return -1;
   }
 }
+
+// Solution2: Left Most Binary Search
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length;
+
+        while (l < r)
+        {
+            int m = (l + r) / 2;
+            if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        if (l >= nums.length || nums[l] != target) return -1;
+        else return l;
+    }
+}
