07ddb489eb65b72a99153366dea63050feee189a
Shota Kishimoto <shokishimo@gmail.com>
2023-05-04 16:37:39 -0500

    Time: 0 ms (100%), Space: 40.2 MB (63.99%) - LeetCode

--- 0226-Invert-Binary-Tree/0226-Invert-Binary-Tree.java
+++ 0226-Invert-Binary-Tree/0226-Invert-Binary-Tree.java
@@ -24,3 +24,19 @@ class Solution {
       return root;
   }
 }
+
+
+// Solution 2
+class Solution {
+  public TreeNode invertTree(TreeNode root) {
+      if (root == null) return root;
+      TreeNode temp = root.left;
+      root.left = root.right;
+      root.right = temp;
+
+      TreeNode left = invertTree(root.left);
+      TreeNode right = invertTree(root.right);
+
+      return root;
+  }
+}
