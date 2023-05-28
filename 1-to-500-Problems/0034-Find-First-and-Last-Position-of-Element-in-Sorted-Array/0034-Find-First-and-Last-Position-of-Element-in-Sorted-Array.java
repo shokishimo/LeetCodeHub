c6d203bde586780118120f5352a71be395af77e3
Shota Kishimoto <shokishimo@gmail.com>
2023-05-27 21:25:21 -0500

    Time 0 ms (100%), Space: 44.7 MB (94.50%) - LeetCode

--- 1-to-500-Problems/0034-Find-First-and-Last-Position-of-Element-in-Sorted-Array/0034-Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
+++ 1-to-500-Problems/0034-Find-First-and-Last-Position-of-Element-in-Sorted-Array/0034-Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
@@ -34,3 +34,33 @@ class Solution {
         return i;
     }
 }
+
+
+
+// Solution2
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) return new int[] {-1, -1};
+        int leftPos = Binary(nums, target, true);
+        if (leftPos == -1) return new int[] {-1, -1};
+        int rightPos = Binary(nums, target+1, false);
+        rightPos = (rightPos == 0)? 0: rightPos-1;
+        return new int[] {leftPos, rightPos};
+    }
+
+    private int Binary(int[] nums, int target, boolean check) {
+        int l = 0;
+        int r = nums.length;
+
+        while (l < r) {
+            int m = (l+r)/2;
+            if (nums[m] < target) 
+                l = m + 1;
+            else 
+                r = m;
+        }
+        if (check && l >= nums.length)  return -1;
+        else if (check && nums[l] != target) return -1;
+        return l;
+    }
+}
