cd223b0c9711bb41a7a45befe7d4c2009fb61002
Shota Kishimoto <shokishimo@gmail.com>
2023-05-28 18:14:37 -0500

    Time 6 ms (44.63%), Space: 45.4 MB (5.86%) - LeetCode

--- 1-to-500-Problems/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.java
+++ 1-to-500-Problems/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.java
@@ -9,27 +9,26 @@
  */
 
  class Solution {
-  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-      TreeNode[] res = findLCA(root, p, q);
-      return res[0];
-  }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode[] res = findLCA(root, p, q);
+        return res[0];
+    }
 
-  private TreeNode[] findLCA(TreeNode root, TreeNode p, TreeNode q) {
-      if (root == null) return new TreeNode[] {null, null, null};
-      
+    private TreeNode[] findLCA(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return new TreeNode[] {null, null, null};
 
-      TreeNode[] left = findLCA(root.left, p, q);
-      TreeNode[] right = findLCA(root.right, p, q);
+        TreeNode[] left = findLCA(root.left, p, q);
+        TreeNode[] right = findLCA(root.right, p, q);
 
-      TreeNode[] cur = left;
-      if (cur[0] == null && right[0] != null) cur[0] = right[0];
-      if (cur[1] == null && right[1] != null) cur[1] = right[1];
-      if (cur[2] == null && right[2] != null) cur[2] = right[2];
+        TreeNode[] cur = left;
+        if (cur[0] == null && right[0] != null) cur[0] = right[0];
+        if (cur[1] == null && right[1] != null) cur[1] = right[1];
+        if (cur[2] == null && right[2] != null) cur[2] = right[2];
 
-      if (cur[1] == null && root.val == p.val) cur[1] = root;
-      if (cur[2] == null && root.val == q.val) cur[2] = root;
-      if (cur[0] == null && cur[1] != null && cur[2] != null) cur[0] = root;
-      
-      return cur;
-  }
+        if (cur[1] == null && root == p) cur[1] = root;
+        if (cur[2] == null && root == q) cur[2] = root;
+        if (cur[0] == null && cur[1] != null && cur[2] != null) cur[0] = root;
+        
+        return cur;
+    }
 }
