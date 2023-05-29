be18bd6dded90f828da213d50f5984c5e8248a31
Shota Kishimoto <shokishimo@gmail.com>
2023-05-28 19:09:53 -0500

    Time 6 ms (44.63%), Space: 44.4 MB (5.86%) - LeetCode

--- 1-to-500-Problems/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.java
+++ 1-to-500-Problems/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.java
@@ -32,3 +32,28 @@
         return cur;
     }
 }
+
+
+// Better solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+
+        while (cur != null) {
+            if (p.val > cur.val && q.val > cur.val) return lowestCommonAncestor(cur.right, p, q);
+            else if (p.val < cur.val && q.val < cur.val) return lowestCommonAncestor(cur.left, p, q);
+            else return cur;
+        }
+        return cur;
+    }
+}
