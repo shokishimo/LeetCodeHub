638cfb4f0860f9a079f0d7bcc1a415ff06d87868
Shota Kishimoto <shokishimo@gmail.com>
2023-06-03 18:15:51 -0500

    Time: 13 ms (6.7%), Space: 46.3 MB (10.30%) - LeetCode

---
+++ 1-to-500-Problems/0130-Surrounded-Regions/0130-Surrounded-Regions.java
@@ -0,0 +1,38 @@
+class Solution {
+  HashSet<List<Integer>> visited;
+  int row;
+  int col;
+
+  public void solve(char[][] board) {
+      row = board.length;
+      col = board[0].length;
+      visited = new HashSet<>();
+
+      for (int c = 0; c < col; c++) {
+          dfs(board, 0, c);
+          dfs(board, row-1, c);
+      }
+      for (int r = 0; r < row; r++) {
+          dfs(board, r, 0);
+          dfs(board, r, col-1);
+      }
+
+      for (int r = 0; r < row; r++) {
+          for (int c = 0; c < col; c++) {
+              if (board[r][c] == 'O' && !visited.contains(Arrays.asList(r, c)))
+                  board[r][c] = 'X';
+          }
+      }
+  }
+
+  private void dfs(char[][] board, int r, int c) {
+      if (r < 0 || r >= row || c < 0 || c >= col || board[r][c] == 'X' 
+          || visited.contains(Arrays.asList(r, c))) return;
+      
+      visited.add(Arrays.asList(r, c));
+      dfs(board, r+1, c);
+      dfs(board, r-1, c);
+      dfs(board, r, c-1);
+      dfs(board, r, c+1);
+  }
+}
