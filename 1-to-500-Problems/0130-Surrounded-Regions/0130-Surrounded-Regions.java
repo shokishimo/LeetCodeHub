da468944a6253fd17b4369aa50e8cca462c8d8e8
Shota Kishimoto <shokishimo@gmail.com>
2023-06-03 18:23:16 -0500

    Time: 1 ms (98.71%), Space: 44.2 MB (88.50%) - LeetCode

--- 1-to-500-Problems/0130-Surrounded-Regions/0130-Surrounded-Regions.java
+++ 1-to-500-Problems/0130-Surrounded-Regions/0130-Surrounded-Regions.java
@@ -36,3 +36,42 @@ class Solution {
       dfs(board, r, c+1);
   }
 }
+
+// Better and efficient way
+class Solution {
+    int row;
+    int col;
+
+    public void solve(char[][] board) {
+        row = board.length;
+        col = board[0].length;
+
+        for (int c = 0; c < col; c++) {
+            dfs(board, 0, c);
+            dfs(board, row-1, c);
+        }
+        for (int r = 0; r < row; r++) {
+            dfs(board, r, 0);
+            dfs(board, r, col-1);
+        }
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (board[r][c] == 'O')
+                    board[r][c] = 'X';
+                if (board[r][c] == 'T')
+                    board[r][c] = 'O';
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int r, int c) {
+        if (r < 0 || r >= row || c < 0 || c >= col || board[r][c] != 'O') return;
+        
+        board[r][c] = 'T';
+        dfs(board, r+1, c);
+        dfs(board, r-1, c);
+        dfs(board, r, c-1);
+        dfs(board, r, c+1);
+    }
+}
