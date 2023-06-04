0009c5f1f624c09075136a7acf556d7fa33a5af3
Shota Kishimoto <shokishimo@gmail.com>
2023-06-04 17:14:58 -0500

    Time: 108 ms (5%), Space: 45.5 MB (5.29%) - LeetCode

---
+++ 1-to-500-Problems/0207-Course-Schedule/0207-Course-Schedule.java
@@ -0,0 +1,47 @@
+class Solution {
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+      Node[] courses = new Node[numCourses];
+      for (int i = 0; i < courses.length; i++) {
+          courses[i] = new Node(i);
+      }
+      for (int i = 0; i < prerequisites.length; i++) {
+          int[] one = prerequisites[i];
+          courses[one[0]].prereq.put(one[1], courses[one[1]]);
+      }
+
+      Deque<Node> que = new ArrayDeque<>();
+      int count = numCourses;
+      while(count > 0) {
+          for (Node each: courses) {
+              if (!each.taken && each.prereq.size() == 0) {
+                  que.add(each);
+              }
+          }
+          if (que.size() == 0) return false;
+      
+          while(!que.isEmpty()) {
+              Node t = que.poll();
+              t.taken = true;
+              for (Node each: courses) {
+                  if (each.prereq.get(t.val) != null) {
+                      each.prereq.remove(t.val);
+                  }
+              }
+              count--;
+          }
+      }
+      return true;
+  }
+}
+
+class Node {
+  int val;
+  boolean taken;
+  HashMap<Integer, Node> prereq;
+
+  Node(int value) {
+      val = value;
+      taken = false;
+      prereq = new HashMap<>();
+  }
+}
