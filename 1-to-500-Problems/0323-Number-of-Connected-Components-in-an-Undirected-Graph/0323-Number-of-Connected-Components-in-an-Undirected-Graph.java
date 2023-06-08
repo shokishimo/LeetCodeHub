790d445c2ab34124bd37f8600965b4ad706bd208
Shota Kishimoto <shokishimo@gmail.com>
2023-06-07 23:54:01 -0500

    Time: 1 ms (100%), Space: 43.9 MB (25.79%) - LeetCode

--- 1-to-500-Problems/0323-Number-of-Connected-Components-in-an-Undirected-Graph/0323-Number-of-Connected-Components-in-an-Undirected-Graph.java
+++ 1-to-500-Problems/0323-Number-of-Connected-Components-in-an-Undirected-Graph/0323-Number-of-Connected-Components-in-an-Undirected-Graph.java
@@ -1,4 +1,4 @@
-// Solution 1
+// Solution 1, Using DFS, less efficient
 class Solution {
     List<List<Integer>> graph;
     HashSet<Integer> visited;
@@ -30,4 +30,50 @@ class Solution {
     }
 }
 
-// Solution 2
+// Solution 2, Using UnionFind, more effient
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] par = new int[n];
+        int[] rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+            rank[i] = 1;
+        }
+
+        for (int[] edge: edges) {
+            union(edge[0], edge[1], par, rank);
+        }
+        int num = 0;
+        for (int each: rank) {
+            if (each != 0) num++;
+        }
+
+        return num;
+    }
+
+    private void union(int a, int b, int[] par, int[] rank) {
+        int A = getPar(a, par);
+        int B = getPar(b, par);
+
+        if (A == B) return;
+        if (rank[A] >= rank[B]) {
+            rank[A] += rank[B];
+            rank[B] = 0;
+            par[B] = A;
+        } else {
+            rank[B] += rank[A];
+            rank[A] = 0;
+            par[A] = B;
+        }
+    }
+
+    private int getPar(int p, int[] par) {
+        p = par[p];
+
+        while (p != par[p]) {
+            par[p] = par[par[p]];
+            p = par[p];
+        }
+        return p;
+    }
+}
