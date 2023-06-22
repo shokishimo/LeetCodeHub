ddb952e2c0cf52c55f455d40b50c7b525bde7a91
Shota Kishimoto <shokishimo@gmail.com>
2023-06-22 02:35:29 -0500

    Time: 65 ms (63.88%), Space: 43.2 MB (46.6%) - LeetCode

---
+++ 1-to-500-Problems/0300-Longest-Increasing-Subsequence/0300-Longest-Increasing-Subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max = 1;
+
+        for (int i = nums.length-1; i >= 0; i--) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] < nums[j]) {
+                    dp[i] = Math.max(dp[i], 1+dp[j]);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
