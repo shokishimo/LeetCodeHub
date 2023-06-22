90a46a2d25655ed1227ad332ef7343439412a124
Shota Kishimoto <shokishimo@gmail.com>
2023-06-22 03:17:05 -0500

    Time: 161 ms (14.49%), Space: 43.9 MB (16.80%) - LeetCode

--- 1-to-500-Problems/0300-Longest-Increasing-Subsequence/0300-Longest-Increasing-Subsequence.java
+++ 1-to-500-Problems/0300-Longest-Increasing-Subsequence/0300-Longest-Increasing-Subsequence.java
@@ -1,3 +1,4 @@
+// Bottom up DP
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
@@ -13,5 +14,39 @@ class Solution {
             max = Math.max(max, dp[i]);
         }
         return max;
+    }
+}
+
+// Top-to-down Recursive (not efficient enough)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        Integer[] dp = new Integer[nums.length];
+        boolean[] determined = new boolean[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            recursive(nums, dp, i, determined);
+        }
+
+        int max = 1;
+        for (int each: dp) {
+            max = Math.max(max, each);
+        }
+        return max;
+    }
+
+    private void recursive(int[] nums, Integer[] dp, int cur, boolean[] determined) {
+        if (cur == nums.length-1) {
+            dp[cur] = 1;
+            return;
+        }
+        if (determined[cur]) return;
+
+        dp[cur] = 1;
+        for (int i = cur+1; i < nums.length; i++) {
+            if (nums[cur] < nums[i]) {
+                recursive(nums, dp, i, determined);
+                dp[cur] = Math.max(dp[cur], 1+dp[i]);
+            }
+        }
+        determined[cur] = true;
     }
 }
