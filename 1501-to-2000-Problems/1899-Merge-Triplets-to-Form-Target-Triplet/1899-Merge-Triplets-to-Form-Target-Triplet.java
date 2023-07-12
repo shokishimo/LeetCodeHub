89382d6a142e4e1176a34c4ad060c66cf1eea646
Shota Kishimoto <shokishimo@gmail.com>
2023-07-11 23:52:38 -0500

    Time 1 ms (99.26%), Space: 119.5 MB (41.64%) - LeetCode

--- 1501-to-2000-Problems/1899-Merge-Triplets-to-Form-Target-Triplet/1899-Merge-Triplets-to-Form-Target-Triplet.java
+++ 1501-to-2000-Problems/1899-Merge-Triplets-to-Form-Target-Triplet/1899-Merge-Triplets-to-Form-Target-Triplet.java
@@ -2,15 +2,14 @@ class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         int[] cur = new int[3];
         for (int i = 0; i < triplets.length; i++) {
-            int[] temp = triplets[i];
-            if (temp[0] > target[0] || temp[1] > target[1] || temp[2] > target[2])
+            if (triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2])
                 continue;
-            if (temp[0] == target[0] && temp[1] == target[1] && temp[2] == target[2])
+            if (triplets[i][0] == target[0] && triplets[i][1] == target[1] && triplets[i][2] == target[2])
                 return true;
 
-            cur[0] = Math.max(cur[0], temp[0]);
-            cur[1] = Math.max(cur[1], temp[1]);
-            cur[2] = Math.max(cur[2], temp[2]);
+            cur[0] = Math.max(cur[0], triplets[i][0]);
+            cur[1] = Math.max(cur[1], triplets[i][1]);
+            cur[2] = Math.max(cur[2], triplets[i][2]);
             if (cur[0] == target[0] && cur[1] == target[1] && cur[2] == target[2])
                 return true;
         }
