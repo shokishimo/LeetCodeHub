2988af1e6237aeed969ecc08132121683ca2bc5a
Shota Kishimoto <shokishimo@gmail.com>
2023-07-13 00:05:19 -0500

    [763. Partition Labels] better way

--- 501-to-1000-Problems/0763-Partition-Labels/0763-Partition-Labels.java
+++ 501-to-1000-Problems/0763-Partition-Labels/0763-Partition-Labels.java
@@ -29,3 +29,27 @@ class Solution {
         return ans;
     }
 }
+
+// solution 2 better
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), i);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int size = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            size++;
+            end = Math.max(end, map.get(s.charAt(i)));
+            if (i == end) {
+                ans.add(size);
+                size = 0;
+            }
+        }
+
+        return ans;
+    }
+}
