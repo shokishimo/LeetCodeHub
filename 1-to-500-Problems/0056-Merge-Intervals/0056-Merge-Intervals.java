ed7880048e752cd772ae12d1276fde176fb8d3b3
Shota Kishimoto <shokishimo@gmail.com>
2023-07-19 03:32:16 -0500

    Time: 9 ms (81.87%), Space: 45.65 MB (52.06%)

---
+++ 1-to-500-Problems/0056-Merge-Intervals/0056-Merge-Intervals.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> ans = new ArrayList<>();
+        int[] newInterval = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                ans.add(newInterval);
+                newInterval = intervals[i];
+            } else {
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+        }
+        ans.add(newInterval);
+        return ans.toArray(new int[ans.size()][2]);
+    }
+}
