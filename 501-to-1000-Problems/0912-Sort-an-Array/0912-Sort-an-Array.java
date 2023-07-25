b2ae07b51345ebeb6d758f2f1ee58460a522d130
Shota Kishimoto <shokishimo@gmail.com>
2023-07-25 04:22:30 -0500

    Time: 119 ms (13.14%), Space: 54.98% MB (75.10%)

---
+++ 501-to-1000-Problems/0912-Sort-an-Array/0912-Sort-an-Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.add(num);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = pq.poll();
+        }
+
+        return nums;
+    }
+}
