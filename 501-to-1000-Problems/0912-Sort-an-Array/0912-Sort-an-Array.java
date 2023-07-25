f40de821ab1a2904dc71b1be0d5c582877979d54
Shota Kishimoto <shokishimo@gmail.com>
2023-07-25 04:47:40 -0500

    Time: 34 ms (78.16%), Space: 56.28 MB (23.59%)

--- 501-to-1000-Problems/0912-Sort-an-Array/0912-Sort-an-Array.java
+++ 501-to-1000-Problems/0912-Sort-an-Array/0912-Sort-an-Array.java
@@ -1,3 +1,4 @@
+// Solution 1, using PriorityQueue
 class Solution {
     public int[] sortArray(int[] nums) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
@@ -9,5 +10,49 @@ class Solution {
         }
 
         return nums;
+    }
+}
+
+// Solution 2, using merge sort
+class Solution {
+    public int[] sortArray(int[] nums) {
+        // merge sort
+        return mergeSort(nums, 0, nums.length-1);
+    }
+
+    private int[] mergeSort(int[] nums, int l, int r) {
+        if (l == r) return new int[] { nums[l] };
+        int mid = (l+r)/2;
+        int[] leftArr = margeSort(nums, l, mid);
+        int[] rightArr = margeSort(nums, mid+1, r);
+
+        int[] ans = new int[leftArr.length + rightArr.length];
+        int left = 0;
+        int right = 0;
+        int index = 0;
+        while (left < leftArr.length && right < rightArr.length) {
+            if (leftArr[left] <= rightArr[right]) {
+                ans[index] = leftArr[left];
+                left++;
+            } else {
+                ans[index] = rightArr[right];
+                right++;
+            }
+            index++;
+        }
+
+        while (left < leftArr.length) {
+            ans[index] = leftArr[left];
+            left++;
+            index++;
+        }
+
+        while (right < rightArr.length) {
+            ans[index] = rightArr[right];
+            right++;
+            index++;
+        }
+
+        return ans;
     }
 }
