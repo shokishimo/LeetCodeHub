b995973e07968dadff0a919cb64d12565e3ec40f
Shota Kishimoto <shokishimo@gmail.com>
2023-07-30 01:45:52 -0500

    Redo: Time: 15 ms (90.40%), Space: 43.33 MB (64.10%)

--- 1-to-500-Problems/0322-Coin-Change/0322-Coin-Change.java
+++ 1-to-500-Problems/0322-Coin-Change/0322-Coin-Change.java
@@ -1,18 +1,16 @@
 class Solution {
-  public int coinChange(int[] coins, int amount) {
-      int[] dp = new int[amount+1];
-      for (int i = 1; i < dp.length; i++) {
-          dp[i] = amount + 1;
-      }
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
 
-      for (int i = 1; i < amount+1; i++) {
-          for (int coin: coins) {
-              if (i - coin >= 0) {
-                  dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
-              }
-          }
-      }
-      if (dp[amount] == amount+1) return -1;
-      return dp[amount];
-  }
+        for (int target = 1; target < dp.length; target++) {
+            for (int coin : coins) {
+                if (target - coin < 0) continue;
+                dp[target] = Math.min(dp[target], 1+dp[target - coin]);
+            }
+        }
+
+        return dp[amount] == amount+1 ? -1 : dp[amount];
+    }
 }
