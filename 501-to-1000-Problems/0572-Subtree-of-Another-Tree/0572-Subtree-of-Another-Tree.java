4ab3365b28d187197585f805acafc0921912886d
Shota Kishimoto <shokishimo@gmail.com>
2023-08-01 04:59:17 -0500

    Time: 2 ms (99.3%), Space: 43.6 MB (28.8%)

--- 501-to-1000-Problems/0572-Subtree-of-Another-Tree/0572-Subtree-of-Another-Tree.java
+++ 501-to-1000-Problems/0572-Subtree-of-Another-Tree/0572-Subtree-of-Another-Tree.java
@@ -33,3 +33,24 @@ class Solution {
       return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
   }
 }
+
+// resolve
+class Solution {
+    TreeNode sub;
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) return true;
+        if (root == null) return false;
+
+        if (isSameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) ||
+                isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode tree, TreeNode subTree) {
+        if (tree == null && subTree == null) return true;
+        if (tree == null || subTree == null || tree.val != subTree.val) return false;
+
+        return isSameTree(tree.left, subTree.left) &&
+                isSameTree(tree.right, subTree.right);
+    }
+}
