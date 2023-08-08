1ae65e7a48ed89054181212fa8b74ace5b5601f9
Shota Kishimoto <shokishimo@gmail.com>
2023-08-08 12:06:02 -0500

    Time: 121 ms (95.56%), Space: 40.7 MB (59.12%)

--- 1-to-500-Problems/0079-Word-Search/0079-Word-Search.java
+++ 1-to-500-Problems/0079-Word-Search/0079-Word-Search.java
@@ -57,3 +57,33 @@ class Solution {
 }
 
 
+// better
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (recursiveSerach(board, word, 0, i, j))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean recursiveSerach(char[][] board, String word, int ind, int i, int j) {
+        if (ind >= word.length()) return true;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length
+                || word.charAt(ind) != board[i][j])
+            return false;
+
+        board[i][j] += 100;
+        boolean res  = recursiveSerach(board, word, ind+1, i-1, j)
+                || recursiveSerach(board, word, ind+1, i+1, j)
+                || recursiveSerach(board, word, ind+1, i, j+1)
+                || recursiveSerach(board, word, ind+1, i, j-1);
+        board[i][j] -= 100;
+        return res;
+    }
+}
+
+
