8d28506883083e63bf2f7237bb201cc80f78e497
Shota Kishimoto <shokishimo@gmail.com>
2023-08-08 12:00:44 -0500

    Time: 1624 ms (5.1%), Space: 44.1 MB (15.56%)

--- 1-to-500-Problems/0079-Word-Search/0079-Word-Search.java
+++ 1-to-500-Problems/0079-Word-Search/0079-Word-Search.java
@@ -25,3 +25,35 @@ class Solution {
       return res;
   }
 }
+
+// resolve O(n * m * 4^(k)), where n = board.length, m = board[0].length, k = len(word)
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (recursiveSerach(board, word, new HashSet<>(), 0, i, j))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean recursiveSerach(char[][] board, String word, HashSet<String> set, int ind, int i, int j) {
+        if (ind >= word.length()) return true;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length) return false;
+        if (word.charAt(ind) != board[i][j] || set.contains("row"+i+"col"+j)) return false;
+
+        set.add("row"+i+"col"+j);
+        boolean res  = recursiveSerach(board, word, set, ind+1, i-1, j)
+                || recursiveSerach(board, word, set, ind+1, i+1, j)
+                || recursiveSerach(board, word, set, ind+1, i, j+1)
+                || recursiveSerach(board, word, set, ind+1, i, j-1);
+        set.remove("row"+i+"col"+j);
+        return res;
+    }
+}
+
+
