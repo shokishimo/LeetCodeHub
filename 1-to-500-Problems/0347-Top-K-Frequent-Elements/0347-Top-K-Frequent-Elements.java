3581a28907616e2495c2af89cfe08614cc2a1240
Shota Kishimoto <shokishimo@gmail.com>
2023-08-12 15:35:39 -0500

    Time: 10 ms (96.14%), Space: 47.8 MB (29.5%)

--- 1-to-500-Problems/0347-Top-K-Frequent-Elements/0347-Top-K-Frequent-Elements.java
+++ 1-to-500-Problems/0347-Top-K-Frequent-Elements/0347-Top-K-Frequent-Elements.java
@@ -1,5 +1,5 @@
 // resolve
-
+// O(n log k)
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -15,6 +15,35 @@ class Solution {
         int i = k;
         while (!pq.isEmpty()) {
             ans[--i] = pq.poll().getKey();
+        }
+        return ans;
+    }
+}
+
+// solution 2
+// O(n)
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> freqList[] = new ArrayList[nums.length + 1];
+        for (Map.Entry<Integer, Integer> eachMap : map.entrySet()) {
+            if (freqList[eachMap.getValue()] == null)
+                freqList[eachMap.getValue()] = new ArrayList<>();
+            freqList[eachMap.getValue()].add(eachMap.getKey());
+        }
+
+        int[] ans = new int[k];
+        int ind = 0;
+        for (int i = freqList.length-1; i >= 0; i--) {
+            if (freqList[i] != null) {
+                for (int eachVal : freqList[i]) {
+                    ans[ind++] = eachVal;
+                    if (ind >= k) return ans;
+                }
+            }
         }
         return ans;
     }
