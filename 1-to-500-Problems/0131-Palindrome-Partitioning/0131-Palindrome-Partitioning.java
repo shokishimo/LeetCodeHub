0983242d3431e3b87a58be8173f02d59a7c16fed
Shota Kishimoto <shokishimo@gmail.com>
2023-08-12 11:10:24 -0500

    6 ms (98.85%), Space: 55.4 MB (65.41%)

--- 1-to-500-Problems/0131-Palindrome-Partitioning/0131-Palindrome-Partitioning.java
+++ 1-to-500-Problems/0131-Palindrome-Partitioning/0131-Palindrome-Partitioning.java
@@ -1,33 +1,33 @@
+// resolve
 class Solution {
-  List<List<String>> ans;
-  public List<List<String>> partition(String s) {
-      ans = new ArrayList<>();
-      backtrack(s, new ArrayList<>(), 0);
-      return ans;
-  }
+    List<List<String>> ans;
+    public List<List<String>> partition(String s) {
+        ans = new ArrayList<>();
+        backtrack(s, new ArrayList<>(), 0);
+        return ans;
+    }
 
-  private void backtrack(String s, List<String> cur, int pos) {
-      if (pos >= s.length()) {
-          ans.add(new ArrayList<>(cur));
-          return;
-      }
-      // evey possible partition
-      for (int i = pos; i < s.length(); i++) {
-          if (isPalin(s, pos, i)) {
-              cur.add(s.substring(pos, i+1));
-              backtrack(s, cur, i+1);
-              cur.remove(cur.size()-1);
-          }
-      }
-  }
+    private void backtrack(String s, List<String> curPart, int i) {
+        if (i >= s.length()) {
+            ans.add(new ArrayList<>(curPart));
+            return;
+        }
 
-  private boolean isPalin(String s, int l, int r) {
-      while (l < r) {
-          if (s.charAt(l) != s.charAt(r))
-              return false;
-          l++;
-          r--;
-      }
-      return true;
-  }
+        for (int j = i; j < s.length(); j++) {
+            if (isPalindrome(s, i, j)) {
+                curPart.add(s.substring(i, j+1));
+                backtrack(s, curPart, j+1);
+                curPart.remove(curPart.size()-1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
 }
