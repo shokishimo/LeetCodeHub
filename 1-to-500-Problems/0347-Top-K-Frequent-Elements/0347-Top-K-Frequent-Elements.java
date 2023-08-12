bcda850be454bfa352911affa31152e75af77df5
Shota Kishimoto <shokishimo@gmail.com>
2023-08-12 14:46:20 -0500

    Time: 12 ms (86.1%), Space: 47.6 MB (75.17%)

--- 1-to-500-Problems/0347-Top-K-Frequent-Elements/0347-Top-K-Frequent-Elements.java
+++ 1-to-500-Problems/0347-Top-K-Frequent-Elements/0347-Top-K-Frequent-Elements.java
@@ -1,19 +1,20 @@
+// resolve
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = 
-                new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-        for (Map.Entry<Integer, Integer> mapEntry : map.entrySet()) {
-            pq.add(mapEntry);
-            if (pq.size() > k) pq.remove();
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        for (Map.Entry<Integer, Integer> eachMap : map.entrySet()) {
+            pq.add(eachMap);
+            if (pq.size() > k) pq.poll();
         }
         int[] ans = new int[k];
-        int size = pq.size();
-        for (int i = 0; i < size; i++) {
-            ans[i] = pq.poll().getKey();
+        int i = k;
+        while (!pq.isEmpty()) {
+            ans[--i] = pq.poll().getKey();
         }
         return ans;
     }
