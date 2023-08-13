994f9ef452c73748134efe8a0516f911c92ebf19
Shota Kishimoto <shokishimo@gmail.com>
2023-08-13 13:39:29 -0500

    Time: 12 ms (47.5%), Space: 44.6 MB (20.19%)

---
+++ 1-to-500-Problems/0125-Valid-Palindrome/0125-Valid-Palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        s.chars()
+                .filter(c -> Character.isLetterOrDigit(c))
+                .mapToObj(c -> Character.toLowerCase((char) c))
+                .forEach(sb::append);
+
+        return sb.toString().equals(sb.reverse().toString());
+    }
+}
