35f3de11988edf64c309071b7130533ee9cf8d58
Shota Kishimoto <shokishimo@gmail.com>
2023-08-15 13:47:49 -0500

    Time: 3 ms (90.25%), Space: 47.5 MB (73.69%)

--- 1-to-500-Problems/0200-number-of-islands/0200-number-of-islands.java
+++ 1-to-500-Problems/0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,25 @@
 class Solution {
-    int row;
-    int col;
     public int numIslands(char[][] grid) {
-        int ans = 0;
-        row = grid.length;
-        col = grid[0].length;
-        
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        int numOfIslands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
-                    removeConnectedIsland(grid, i, j);
-                    ans++;
+                    dfs(grid, i, j);
+                    numOfIslands++;
                 }
             }
         }
-        return ans;
+
+        return numOfIslands;
     }
-    
-    private void removeConnectedIsland(char[][] grid, int r, int c) {
-        if (r < 0 || r >= row || c < 0 || c >= col || grid[r][c] == '0') return;
-        grid[r][c] = '0';
-        removeConnectedIsland(grid, r+1, c);
-        removeConnectedIsland(grid, r, c+1);
-        removeConnectedIsland(grid, r, c-1);
-        removeConnectedIsland(grid, r-1, c);
+
+    private void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] != '1') return;
+
+        grid[r][c] = 'v';
+        dfs(grid, r-1, c);
+        dfs(grid, r+1, c);
+        dfs(grid, r, c-1);
+        dfs(grid, r, c+1);
     }
-    
 }
