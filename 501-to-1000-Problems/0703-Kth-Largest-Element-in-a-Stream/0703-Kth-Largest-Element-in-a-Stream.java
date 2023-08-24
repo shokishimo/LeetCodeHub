fa2cf3eff9df99003465e75da0c39ecd0cf5b072
Shota Kishimoto <shokishimo@gmail.com>
2023-08-23 19:18:10 -0500

    Time: 15 ms (68.34%), Space: 49.1 MB (10.51%)

--- 501-to-1000-Problems/0703-Kth-Largest-Element-in-a-Stream/0703-Kth-Largest-Element-in-a-Stream.java
+++ 501-to-1000-Problems/0703-Kth-Largest-Element-in-a-Stream/0703-Kth-Largest-Element-in-a-Stream.java
@@ -1,27 +1,26 @@
+// resolve
 class KthLargest {
-  int k;
-  PriorityQueue<Integer> pq;
-  
-  public KthLargest(int k, int[] nums) {
-      this.k = k;
-      this.pq = new PriorityQueue<>();
-      for (int num: nums) {
-          pq.add(num);
-      }
-      while(k < pq.size()) {
-          pq.poll();
-      }
-  }
-  
-  public int add(int val) {
-      pq.add(val);
-      while (k < pq.size()) pq.poll();
-      return pq.peek();
-  }
+    PriorityQueue<Integer> pqMin;
+    int theK;
+
+    public KthLargest(int k, int[] nums) {
+        pqMin = new PriorityQueue<>((a, b) -> a - b);
+        theK = k;
+        for (int num : nums) {
+            pqMin.add(num);
+            if (pqMin.size() > k) pqMin.poll();
+        }
+    }
+
+    public int add(int val) {
+        pqMin.add(val);
+        if (pqMin.size() > theK) pqMin.poll();
+        return pqMin.peek();
+    }
 }
 
 /**
-* Your KthLargest object will be instantiated and called as such:
-* KthLargest obj = new KthLargest(k, nums);
-* int param_1 = obj.add(val);
-*/
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
