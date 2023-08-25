038ffb9efde85b4e60bffaf20a4decafa905afb2
Shota Kishimoto <shokishimo@gmail.com>
2023-08-25 01:59:40 -0500

    Time: 5 ms (62.70%), Space: 44.1 MB (59.83%)

--- 1-to-500-Problems/0207-Course-Schedule/0207-Course-Schedule.java
+++ 1-to-500-Problems/0207-Course-Schedule/0207-Course-Schedule.java
@@ -1,81 +1,32 @@
 class Solution {
-  public boolean canFinish(int numCourses, int[][] prerequisites) {
-      Node[] courses = new Node[numCourses];
-      for (int i = 0; i < courses.length; i++) {
-          courses[i] = new Node(i);
-      }
-      for (int i = 0; i < prerequisites.length; i++) {
-          int[] one = prerequisites[i];
-          courses[one[0]].prereq.put(one[1], courses[one[1]]);
-      }
-
-      Deque<Node> que = new ArrayDeque<>();
-      int count = numCourses;
-      while(count > 0) {
-          for (Node each: courses) {
-              if (!each.taken && each.prereq.size() == 0) {
-                  que.add(each);
-              }
-          }
-          if (que.size() == 0) return false;
-      
-          while(!que.isEmpty()) {
-              Node t = que.poll();
-              t.taken = true;
-              for (Node each: courses) {
-                  if (each.prereq.get(t.val) != null) {
-                      each.prereq.remove(t.val);
-                  }
-              }
-              count--;
-          }
-      }
-      return true;
-  }
-}
-
-class Node {
-  int val;
-  boolean taken;
-  HashMap<Integer, Node> prereq;
-
-  Node(int value) {
-      val = value;
-      taken = false;
-      prereq = new HashMap<>();
-  }
-}
-
-// more efficient solution
-class Solution {
-    HashMap<Integer, ArrayList<Integer>> map;
-    HashSet<Integer> visited;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        map = new HashMap<>();
+        List<List<Integer>> prereq = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
-            map.put(i, new ArrayList<>());
+            prereq.add(new ArrayList<>());
         }
-        for (int[] course: prerequisites) {
-            map.get(course[0]).add(course[1]);
-        }
-        visited = new HashSet<>();
 
-        for(int i = 0; i < numCourses; i++) {
-            if (!dfs(i)) return false;
+        int[] taken = new int[numCourses];
+        for (int[] eachPre : prerequisites) {
+            prereq.get(eachPre[1]).add(eachPre[0]);
+            taken[eachPre[0]]++;
         }
-        return true;
-    }
 
-    private boolean dfs(int cur) {
-        if (visited.contains(cur)) return false;
-        if (map.get(cur).size() == 0) return true;
-
-        visited.add(cur);
-        for (int each: map.get(cur)) {
-            if (!dfs(each)) return false;
+        Deque<Integer> starts = new LinkedList<>();
+        for (int i = 0; i < taken.length; i++) {
+            if (taken[i] == 0) starts.add(i);
         }
-        visited.remove(cur);
-        map.get(cur).clear();
-        return true;
+
+        int numTaken = 0;
+        while (!starts.isEmpty()) {
+            int ind = starts.poll();
+            numTaken++;
+            for (int nextCourseInd : prereq.get(ind)) {
+                taken[nextCourseInd]--;
+                if (taken[nextCourseInd] == 0)
+                    starts.add(nextCourseInd);
+            }
+        }
+
+        return numTaken == numCourses;
     }
 }
