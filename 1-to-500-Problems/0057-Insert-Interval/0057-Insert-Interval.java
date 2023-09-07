7229abdc24156d4234034953a478147c9ac410cf
Shota Kishimoto <shokishimo@gmail.com>
2023-09-07 13:39:49 -0500

    resolve 'insert interval'

--- 1-to-500-Problems/0057-Insert-Interval/0057-Insert-Interval.java
+++ 1-to-500-Problems/0057-Insert-Interval/0057-Insert-Interval.java
@@ -1,20 +1,21 @@
+// resolve
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
+        List<int[]> ans = new ArrayList<>();
         for (int[] interval : intervals) {
+            // non-overlapping
             if (newInterval == null || interval[1] < newInterval[0]) {
-                res.add(interval);
-            } else if (newInterval[1] < interval[0]) {
-                res.add(newInterval);
+                ans.add(interval);
+            } else if (interval[0] > newInterval[1]) {
+                ans.add(newInterval);
+                ans.add(interval);
                 newInterval = null;
-                res.add(interval);
-            } else {
+            } else { // overlapping
                 newInterval[0] = Math.min(newInterval[0], interval[0]);
                 newInterval[1] = Math.max(newInterval[1], interval[1]);
             }
         }
-
-        if (newInterval != null) res.add(newInterval);
-        return res.toArray(new int[res.size()][2]);
+        if (newInterval != null) ans.add(newInterval);
+        return ans.toArray(new int[ans.size()][]);
     }
 }
